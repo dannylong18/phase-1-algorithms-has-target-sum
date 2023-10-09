00186837f0d6c7196b51c1ed9830fbb19a59fb22
Daniel Long <danielwlong618@gmail.com>
2023-10-08 21:50:16 -0500

    lab done

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for ( i=0; i < array.length; i++) {
+    let remainder = target - array[i];
+    for (j=i+1; j < array.length; j++){
+      if (array[j] === remainder)
+      return true
+    }
+  }
+  return false 
 }
 
 /* 
-  Write the Big O time complexity of your function here
+  O(n^2)
 */
 
 /* 
-  Add your pseudocode here
+  loop through initial array starting at the first position and subtract that number from the target;
+  the remainder should be one of the remaining numbers in the array; if so return true; else false
 */
 
 /*
-  Add written explanation of your solution here
+  write function that loops through array and subtract first number from target number; 
+  then we will loop through remainder of array to find that subtracted number; if found, we will return true; else false
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+  console.log ('expecting: true');
+  console.log ('=>', hasTargetSum([1, 4, 9, 10, 15], 13))
+
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
